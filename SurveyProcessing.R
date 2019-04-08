2fffce3f5b83c262e1a64d53dd083d49a58c57b1
Jill MacKay <31953319+jillymackay@users.noreply.github.com>
2019-04-08 19:01:59 +0100

    Update SurveyProcessing.R

--- SurveyProcessing.R
+++ SurveyProcessing.R
@@ -1,8 +1,20 @@
 
 # Recoding Multiple Columns
 
+# Neatest way:
+# named vector of level changes
+levs <- c("Level1" = "0","Level2" = "1", "Level3" = "2")
 
-# Can either create a function like so:
+dat <- tibble(x1 = c(1,2,1,0,1),
+              x2 = c(0,1,2,0,1))
+
+dat %>%
+  mutate_at(vars(x1, x2), factor) %>% 
+  mutate_at(vars(x1, x2), list(~fct_recode(., !!!levs)))
+
+
+
+# Alternatively:
 
 reorder <- function (x) {
   case_when (x == 1 ~ "Disagree",
