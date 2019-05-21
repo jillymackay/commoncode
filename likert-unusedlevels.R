3086d3285f6f81d5910078e64d7a3f3ca792a9b2
Jill MacKay <31953319+jillymackay@users.noreply.github.com>
2019-05-21 13:12:44 +0100

    tidyverse fixes everything eventually

--- likert-unusedlevels.R
+++ likert-unusedlevels.R
@@ -1,92 +1,46 @@
+
+# a tidyverse approach to solving the 
+# 'unused levels' error in the likert package
+
+# See:  https://github.com/jbryer/likert/blob/master/demo/UnusedLevels.R#L6
+
+
+
 library(tidyverse)
 library(likert)
 
 
+dat <- tibble (grp = c("A","B","A","B","A","B"),
+              x1 = c("z", "x", "x", "z", "x", "z"),
+              x2 = c("y", "x", "z", "y", "z", "z"),
+              y1 = c("Y", "X", "Y", "Z", "Z", "Z")) 
 
-# This example which I've modified from 
-# https://github.com/jbryer/likert/blob/master/demo/UnusedLevels.R#L6
-# works
+dat <- as.data.frame(dat)
 
-
-# Create your levels
-l1 <- c("Level1", "Level2", "Level3", "Level4")
-l2 <- c("LevelA", "LevelB")
-
-d <- data.frame(grp=factor(sample(c('g1', 'g2'), 100, replace = TRUE)),
-                x1=factor(sample(l1[1:3], 100, replace=TRUE)),
-                x2=factor(sample(l1[1:4], 100, replace=TRUE)),
-                y1=factor(sample(l2[1:2], 100, replace=TRUE)),
-                check.names=FALSE)
+dlikert1 <- likert (items = dat[,2:3], grouping = dat[,1])
+dlikert2 <- likert (items = dat[,4, drop = FALSE], grouping = dat[,1])
+# Both return an error that all items (columns) must have the same number of levels
 
 
-sapply(d, class) # All columns are factors
-sapply(d, function(x) { length(levels(x)) } ) # The number of levels in each factor
+
+# The Solution:
+
+dat2 <- dat %>% 
+  mutate_at (.vars = vars (x1:x2),
+             parse_factor, levels = c("x",
+                                      "y",
+                                      "z")) %>% 
+  mutate_at (.vars = vars(y1),
+             parse_factor, levels = c("X",
+                                      "Y",
+                                      "Z"))
+
+dat2 <- as.data.frame(dat2)
 
 
-# note you don't need seq_along like jbryer's example
-for(i in 2:3) {
-  d[,i] <- factor(d[,i], levels=l1)
-}
-
-for(i in 4) {
-  d[,i] <- factor(d[,i], levels=l2)
-}
-
-
-dlikert1 <- likert (items = d[,2:3], grouping = d[,1])
-dlikert2 <- likert (items = d[,4, drop = FALSE], grouping = d[,1])
-
-
-plot(dlikert1) 
-# Note that all 4 levels appear in legend while not present in x1
-# This is good! 
-
-plot(dlikert2)
-# And the simple one works too. 
-
-
-# And a tidyverse tibbles approach that also works!:
-
-d2 <- tibble (grp = c("A","B","A","B","A","B"),
-              x1 = c("0", "1", "2", "1", "2", "1"),
-              x2 = c("0", "2", "1", "3", "1", "1"),
-              y1 = c("0", "1", "0", "1", "1", "1")) %>%
-  mutate_at(.vars = vars (x1:x2),
-            .funs = function (x) case_when (x == '0' ~ "Level1",
-                                            x == '1' ~ "Level2",
-                                            x == '2' ~ "Level3",
-                                            x == '3' ~ "Level4",
-                                            TRUE ~ as.character(x))) %>%
-  mutate_at(.vars = vars (y1),
-            .funs = function (x) case_when (x == '0' ~ "LevelA",
-                                            x == '1' ~ "LevelB",
-                                            TRUE ~ as.character(x))) %>%
-  mutate_all(factor) 
-
-d2 <- as.data.frame(d2)
-
-
-sapply(d2, class) #Verify that all the columns are indeed factors
-sapply(d2, function(x) { length(levels(x)) } ) # The number of levels in each factor
-
-l1 <- c("Level1", "Level2", "Level3", "Level4")
-l2 <- c("LevelA", "LevelB")
-
-
-for(i in 2:3) {
-  d2[,i] <- factor(d2[,i], levels=l1)
-}
-
-for(i in 4) {
-  d2[,i] <- factor(d2[,i], levels=l2)
-}
-
-
-sapply(d2, function(x) { length(levels(x)) } ) # Observe difference in factor levels
-
-
-d2likert1 <- likert (items = d2[,2:3], grouping = d2[,1])
-d2likert2 <- likert (items = d2[,4, drop = FALSE], grouping = d2[,1])
+# Now runs beautifully
+d2likert1 <- likert (items = dat2[,2:3], grouping = dat2[,1])
+d2likert2 <- likert (items = dat2[,4, drop = FALSE], grouping = dat2[,1])
 
 plot(d2likert1)
 plot(d2likert2)
